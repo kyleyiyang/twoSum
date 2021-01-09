c619fd500c4f60a5a8b7da79bb3284c845b369ac
kyleyiyang <kyleyiyang@gmail.com>
2021-01-08 23:59:22 -0500

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,11 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
         int[] ans=new int[2];
-        for (int i=0;i<nums.length-1;i++) {
-            for (int j=i+1;j<nums.length;j++) {
-                if (nums[j]+nums[i]==target) {
-                    ans[0]=i;
-                    ans[1]=j;
+        for (int i=0;i<nums.length;i++) {
+            map.put(nums[i],i);
+        }
+        //System.out.println(map);
+        for (int i=0;i<nums.length;i++) {
+            if (map.containsKey(target-nums[i])) {
+                ans[0]=i;
+                ans[1]=map.get(target-nums[i]);
+                if (ans[0]!=ans[1]) {
                     return ans;
                 }
             }
